73fe75f3ae17f5cb2ac40cfd62898861192262e9
dario-piotrowicz <dario.piotrowicz@gmail.com>
2023-08-19 19:15:21 +0100

    update tsdoc comment

--- tests/utils/packageManager.ts
+++ tests/utils/packageManager.ts
@@ -3,10 +3,9 @@ import type { PackageManager, PackageManagerName } from 'src/packageManager';
 import { setupFsForTesting } from './fsSetup';
 
 /**
- * Sets up the mock filesystem for a standard
+ * Creates a package manager that can be used to test the various functionalities of a PackageManager object
  *
  * @param pm Name of the package Manager
- * @param options Options for the setup
  */
 export async function getPackageManagerForTesting(pm: PackageManagerName): Promise<PackageManager> {
 	await setupFsForTesting(pm);
