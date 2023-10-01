1cfd3b9fb0f3ebc127ce8d11704366943d275d31
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2023-10-01 17:14:01 +0100

    in pnpm's `getPackageInfo` don't use the json variant as it seems unreliable (#29)

--- src/package/npm.ts
+++ src/package/npm.ts
@@ -1,5 +1,35 @@
-import { getNpmOrPnpmGetPackageInfoFunction } from './shared';
+import shellac from 'shellac';
+import type { GetPackageInfo } from './index';
+
+type NpmListOutputJsonDependency = {
+	version: string;
+	resolved: string;
+};
+
+type NpmListOutputJson = {
+	version: string;
+	name: string;
+	dependencies?: Record<string, NpmListOutputJsonDependency>;
+	devDependencies?: Record<string, NpmListOutputJsonDependency>;
+};
 
 export function getNpmGetPackageInfoFunction() {
-	return getNpmOrPnpmGetPackageInfoFunction('npm');
+	return async (...[name]: Parameters<GetPackageInfo>): ReturnType<GetPackageInfo> => {
+		try {
+			const listOutput = (await shellac`$ npm list --depth=0 --json`).stdout;
+
+			const outputJson = JSON.parse(listOutput);
+
+			const info: NpmListOutputJson = outputJson;
+			const packageInfo = info.dependencies?.[name] ?? info.devDependencies?.[name];
+
+			if (!packageInfo) {
+				return null;
+			}
+
+			return { name, version: packageInfo.version };
+		} catch {
+			throw new Error(`An error occurred while gathering the package info of "${name}"`);
+		}
+	};
 }
--- src/package/pnpm.ts
+++ src/package/pnpm.ts
@@ -1,5 +1,23 @@
-import { getNpmOrPnpmGetPackageInfoFunction } from './shared';
+import shellac from 'shellac';
+import type { GetPackageInfo } from './index';
 
 export function getPnpmGetPackageInfoFunction() {
-	return getNpmOrPnpmGetPackageInfoFunction('pnpm');
+	return async (...[name]: Parameters<GetPackageInfo>): ReturnType<GetPackageInfo> => {
+		try {
+			const listOutput = (await shellac`$ pnpm list --depth=0`).stdout;
+
+			const versionRegex = new RegExp(`^${name}\\s+(.*)$`, 'im');
+
+			const match = listOutput.match(versionRegex);
+			const version = match?.[1];
+
+			if (!version) {
+				return null;
+			}
+
+			return { name, version };
+		} catch {
+			throw new Error(`An error occurred while gathering the package info of "${name}"`);
+		}
+	};
 }
