65a4f264d3f30a31090a17e9604591da2fb96838
dario-piotrowicz <dario.piotrowicz@gmail.com>
2023-10-15 17:49:52 +0100

    export a single file (so that users can't import from /dist/...)

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { readFileSync, writeFileSync, copyFileSync, readdirSync, statSync } from 'node:fs';
+import { readFileSync, writeFileSync, copyFileSync } from 'node:fs';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
@@ -7,29 +7,14 @@ import { externalizeDeps } from 'vite-plugin-externalize-deps';
 
 const normalizeResolve = (...path: string[]) => resolve(__dirname, ...path).replace(/\\/g, '/');
 
-const getPathsRecursively = (baseDir: string): string[] =>
-	readdirSync(normalizeResolve(baseDir))
-		.map((p) => normalizeResolve(baseDir, p))
-		.map((p) => (statSync(p).isDirectory() ? getPathsRecursively(p) : p))
-		.flat()
-		.filter((p) => !/(\.spec|test)\.tsx?$/gi.test(p))
-		.filter((p) => /\.tsx?$/gi.test(p));
-
-const resolveEntryPath = (path: string, baseDir: string) =>
-	path
-		.replace(normalizeResolve(baseDir), '')
-		.replace(/^\//, '')
-		.replace(/\.tsx?$/g, '');
-
-const getEntryPoints = (baseDir: string) =>
-	Object.fromEntries(getPathsRecursively(baseDir).map((p) => [resolveEntryPath(p, baseDir), p]));
-
 export default defineConfig({
 	build: {
 		outDir: 'dist',
 		emptyOutDir: true,
 		lib: {
-			entry: getEntryPoints('src'),
+			name: 'package-manager-manager',
+			fileName: 'index',
+			entry: 'src/index.ts',
 		},
 		sourcemap: true,
 		minify: 'esbuild',
@@ -41,6 +26,7 @@ export default defineConfig({
 			include: ['src/**/*.{ts,tsx}', 'env.d.ts'],
 			entryRoot: 'src',
 			outputDir: 'dist',
+			rollupTypes: true,
 		}),
 		{
 			name: 'post-build-script',
