4de47b4b260d0df024c2d0261f3d1cde06957b23
dario-piotrowicz <dario.piotrowicz@gmail.com>
2023-10-21 14:08:49 +0100

    Revert "try-catch package manager version detection (#39)"
    This reverts commit 4f1e0d2a219bf459889f9e7b5731d7364b95ef9b.

--- src/packageManager.ts
+++ src/packageManager.ts
@@ -18,7 +18,7 @@ export type PackageManager = {
 	 * project set up using `pnpm`) */
 	projectPackageManager: PackageManagerName | null;
 	/** The version of the package manager */
-	version: string | null;
+	version: string;
 	/**
 	 * Utility to get the information of an installed package
 	 *
@@ -67,15 +67,9 @@ export type PackageManager = {
 	cliCommandKeywords: Set<string>;
 };
 
-async function getPackageManagerVersion(
-	packageManager: PackageManagerName,
-): Promise<string | null> {
-	try {
-		const { stdout } = await shellac`$ ${packageManager} --version`;
-		return stdout;
-	} catch {
-		return null;
-	}
+async function getPackageManagerVersion(packageManager: PackageManagerName): Promise<string> {
+	const { stdout } = await shellac`$ ${packageManager} --version`;
+	return stdout;
 }
 
 /**
