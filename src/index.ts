ff58791c1b11b0c99aaa291873d3ff232c3fbdea
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2023-10-21 14:29:34 +0100

    Types and entrypoint (#38)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 export { getPackageManager } from './packageManager';
 export type { PackageManager, PackageManagerName } from './packageManager';
+
+export type { PackageInfo } from './package';
+
+export type { GetRunScriptOptions } from './commands/getRunScript';
+export type { GetRunExecOptions } from './commands/getRunExec';
+
+export type { CommandScriptStruct, CommandExecStruct } from './commands/CommandStruct';
